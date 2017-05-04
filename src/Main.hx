63b585eb1e2a9a2a18c8f89b6643a0e9f22b8a3a
Dan Korostelev <nadako@gmail.com>
2017-05-04 18:22:16 +0300

    some splitting

--- src/Main.hx
+++ src/Main.hx
@@ -1,77 +1,8 @@
 import protocol.debug.Types;
-import js.node.Buffer;
 import js.node.Net;
-import js.node.stream.Readable;
-import js.node.net.Socket;
 import js.node.ChildProcess;
 import js.node.child_process.ChildProcess.ChildProcessEvent;
-
-typedef Message = {}
-
-class Connection {
-	var socket:Socket;
-	var buffer:Buffer;
-	var index:Int;
-	var nextMessageLength:Int;
-
-	static inline var DEFAULT_BUFFER_SIZE = 4096;
-
-	public function new(socket) {
-		this.socket = socket;
-		buffer = new Buffer(DEFAULT_BUFFER_SIZE);
-		index = 0;
-		nextMessageLength = -1;
-		socket.on(ReadableEvent.Data, onData);
-	}
-
-	function append(data:Buffer) {
-		// append received data to the buffer, increasing it if needed
-		if (buffer.length - index >= data.length) {
-			data.copy(buffer, index, 0, data.length);
-		} else {
-			var newSize = (Math.ceil((index + data.length) / DEFAULT_BUFFER_SIZE) + 1) * DEFAULT_BUFFER_SIZE; // copied from the language-server protocol reader
-			if (index == 0) {
-				buffer = new Buffer(newSize);
-				data.copy(buffer, 0, 0, data.length);
-			} else {
-				buffer = Buffer.concat([buffer.slice(0, index), data], newSize);
-			}
-		}
-		index += data.length;
-	}
-
-	function onData(data:Buffer) {
-		trace(data.toString());
-		append(data);
-		while (true) {
-			if (nextMessageLength == -1) {
-				if (index < 2)
-					return; // not enough data
-				nextMessageLength = buffer.readUInt16LE(0);
-				index -= 2;
-				buffer.copy(buffer, 0, 2);
-			}
-			if (index < nextMessageLength)
-				return;
-			var bytes = buffer.toString("utf-8", 0, nextMessageLength);
-			buffer.copy(buffer, 0, nextMessageLength);
-			index -= nextMessageLength;
-			var json = haxe.Json.parse(bytes);
-			onMessage(json);
-		}
-	}
-
-	public dynamic function onMessage(msg:Message) {}
-
-	public function sendCommand(name:String, ?arg:String) {
-		var cmd = if (arg == null) name else name + " " + arg;
-		var body = Buffer.from(cmd, "utf-8");
-		var header = Buffer.alloc(2);
-		header.writeUInt16LE(body.length, 0);
-		socket.write(header);
-		socket.write(body);
-	}
-}
+import Message;
 
 @:keep
 class Main extends adapter.DebugSession {
@@ -101,7 +32,7 @@ class Main extends adapter.DebugSession {
 			trace("Haxe connected!");
 			var connection = new Connection(socket);
 			connection.onMessage = onMessage;
-			connection.sendCommand("files");
+			sendResponse(response);
 		}
 
 		function onExit(_, _) {
@@ -119,8 +50,12 @@ class Main extends adapter.DebugSession {
 			];
 			var haxeProcess = ChildProcess.spawn("haxe", args, {stdio: Inherit});
 			haxeProcess.on(ChildProcessEvent.Exit, onExit);
-			sendResponse(response);
 		});
+	}
+
+	override function setBreakPointsRequest(response:SetBreakpointsResponse, args:SetBreakpointsArguments) {
+		trace("Setting breakpoints " + args);
+		sendResponse(response);
 	}
 
 	static function main() {
